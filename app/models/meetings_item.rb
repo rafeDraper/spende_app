62727c83909602f2a801f50e1699314a52805280
rafedraper <rafedraper@gmail.com>
2019-10-16 17:10:47 +0200

    Wroking validations

--- app/models/meetings_item.rb
+++ app/models/meetings_item.rb
@@ -1,5 +1,9 @@
 class MeetingsItem < ApplicationRecord
   belongs_to :meetings_list
+  validates :amount, presence: true
+  validates :date, presence: true
+  validates :reason, presence: true
+  validates :con_change, presence: true
 
   def completed?
     !completed_at.blank?
--- app/models/meetings_list.rb
+++ app/models/meetings_list.rb
@@ -1,3 +1,5 @@
 class MeetingsList < ApplicationRecord
   has_many :meetings_items, dependent: :delete_all
+  validates :title, presence: true
+  validates :responsible, presence: true
 end
