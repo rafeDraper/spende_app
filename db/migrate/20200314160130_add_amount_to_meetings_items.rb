68e31c5aec47a7c2e7989b67122569270335f1d7
rafedraper <rafedraper@gmail.com>
2020-03-20 17:19:32 +0100

    updated the money gem and working on clive

--- db/migrate/20200314160130_add_amount_to_meetings_items.rb
+++ db/migrate/20200314160130_add_amount_to_meetings_items.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class AddAmountToMeetingsItems < ActiveRecord::Migration[6.0]
   def change
-    add_monetize :meetings_items, :amount,
-                 numericality: {
-                   greater_than_or_equal_to: 0,
-                   less_than_or_equal_to: 10_000
-                 }
+    add_monetize :meetings_items, :amount, allow_nil: true,
+                                           numericality: {
+                                             greater_than_or_equal_to: 0,
+                                             less_than_or_equal_to: 10_000
+                                           }
   end
 end
