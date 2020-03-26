e1e4f54c14382b596a01075cc21d437ea83049f0
rafedraper <rafedraper@gmail.com>
2020-03-26 13:22:21 +0100

    updated scss, forms and flatpickr

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -2,6 +2,8 @@ import Flatpickr from 'stimulus-flatpickr'
 
 import { German } from 'flatpickr/dist/l10n/de.js'
 
+import 'flatpickr/dist/themes/dark.css'
+
 export default class extends Flatpickr {
   initialize() {
     this.config = {
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("flatpickr/dist/themes/material_green.css")
 require("cleave.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
