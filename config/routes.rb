c25971910b51869ba6852b8f8a2bb2965f5ac2b1
rafedraper <rafedraper@gmail.com>
2020-04-01 13:38:06 +0200

    Added links to contact,help page, and email link

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'static_pages/help'
   get 'static_pages/contact'
+  get 'static_pages/home'
   resources :meetings_lists do
     resources :meetings_items do
       member do
