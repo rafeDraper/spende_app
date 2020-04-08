368e53bdb35c8a446c081ce49e883cad08559fec
rafedraper <rafedraper@gmail.com>
2020-04-08 22:21:46 +0200

    generated new role user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_133139) do
+ActiveRecord::Schema.define(version: 2020_04_08_201945) do
 
   create_table "meetings_items", force: :cascade do |t|
     t.string "date"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_03_30_133139) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider", limit: 50, default: "", null: false
     t.string "uid", limit: 500, default: "", null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
