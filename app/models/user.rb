19db42a4a41147b761a63f1f972ab7d8ab626b41
rafedraper <rafedraper@gmail.com>
2020-04-16 17:12:29 +0200

    method udpated for user default role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  enum role: %i[user editor admin]
-  after_initialize :set_default_role, if: :new_record?
-
-  def set_default_role
-    self.role ||= :user
-  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
+
+  enum role: %i[user editor admin]
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role = :user
+    #self.role ||= :user this method was not working on production
+  end
 
   def self.from_omniauth(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
