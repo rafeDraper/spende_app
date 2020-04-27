86ebc403a3ac8626176b089d57ed172fbf5a07c1
rafedraper <rafedraper@gmail.com>
2020-04-27 15:27:26 +0200

    added come callbacks to the flatpickr

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -10,4 +10,10 @@ export default class extends Flatpickr {
       locale: German
     }
   }
+  // all flatpickr hooks are available as callbacks in your Stimulus controller
+  change(selectedDates, dateStr, instance) {
+    console.log('the callback returns the selected dates', selectedDates)
+    console.log('but returns it also as a string', dateStr)
+    console.log('and the flatpickr instance', instance)
+  }
 }
