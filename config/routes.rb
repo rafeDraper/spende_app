4c232e27723e370e2a79f3348d616fc1e7a8c225
rafedraper <rafedraper@gmail.com>
2020-05-01 23:28:36 +0200

    updated method to scope the user sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   get 'static_pages/help'
   get 'static_pages/contact'
   resources :users
