94008da428934be0b15b719129d5112264b040a4
rafedraper <rafedraper@gmail.com>
2020-05-04 15:11:52 +0200

    testing is a headache!

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ end
 
 group :test do
   gem 'capybara',                 '3.28.0'
-  gem 'guard',                    '2.15.0'
+  gem 'guard',                    '2.16.2'
   gem 'guard-minitest',           '2.4.6'
   gem 'minitest',                 '5.11.3'
   gem 'minitest-reporters',       '1.3.8'
--- test/models/meetings_item_test.rb
+++ test/models/meetings_item_test.rb
@@ -1,15 +1,23 @@
-# frozen_string_literal: true
-
 require 'test_helper'
 
 class MeetingsItemTest < ActiveSupport::TestCase
   def setup
-    @item = MeetingsItem.new(date: '11.11.2020',
-                             reason: 'example_reason',
-                             amount_cents: 22)
+    @meetings_item = meetings_items(:one)
+  end
+  test 'valid meeting item' do
+    assert @meetings_item.valid?
   end
 
-  test 'saving item should be valid' do
-    assert_not @item.valid?
+  test 'monetize present by the item' do
+  end
+
+  test 'invalid without reason' do
+    
+    assert_not_nil @meetings_item.errors[:reason], 'no validation error for reason present'
+  end
+
+  test 'invalid without date' do
+ 
+    assert_not_nil @meetings_item.errors[:date]
   end
 end
