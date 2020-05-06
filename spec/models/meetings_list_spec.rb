52c174876c2e72d19c705a7daa767bee195339e8
rafedraper <rafedraper@gmail.com>
2020-05-06 16:11:30 +0200

    testing models green

--- spec/models/meetings_list_spec.rb
+++ spec/models/meetings_list_spec.rb
@@ -12,6 +12,16 @@ RSpec.describe MeetingsList, type: :model do
       it 'validates the list' do
         expect(meetings_list).to be_valid
       end
+
+      it 'validates to have a title' do
+        meetings_list.title = nil
+        expect(meetings_list).to_not be_valid
+      end
+
+      it 'validates to have a responsible' do
+        meetings_list.responsible = nil
+        expect(meetings_list).to_not be_valid
+      end
     end
   end
 end
