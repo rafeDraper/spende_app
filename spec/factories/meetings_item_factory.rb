e9d280a0bbfaa7a4cf1d788700ab905da1aad165
rafedraper <rafedraper@gmail.com>
2020-05-06 12:52:07 +0200

    testing suite

--- spec/factories/meetings_item_factory.rb
+++ spec/factories/meetings_item_factory.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :meetings_item do
     id     { 2 }
-    date   { "Date of Today"}
-    reason { "Spende" }
+    date   { 'Date of Today' }
+    reason { 'Spende' }
     amount { 22 }
     meetings_list_id { 2 }
   end
---
+++ spec/factories/meetings_list_factory.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :meetings_list do
+    name        { 'Test Name' }
+    responsible { 'Test Responsible' }
+    id          { 1 }
+  end
+end
