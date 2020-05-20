a85c158547a02ae35d8ee3d00ca4661ab442bdc3
rafedraper <rafedraper@gmail.com>
2020-05-20 14:53:47 +0200

    Devise test sign in for user green

--- spec/features/user/sing_in_spec.rb
+++ spec/features/user/sing_in_spec.rb
@@ -1,24 +1,24 @@
 feature 'Sign in', :devise do
-
+  let(:user) { create(:user) }
   scenario 'user cannot sign in if not registered' do
     signin('example@example.com', 'password')
     expect(page).to have_content 'E-Mail oder Passwort ungültig.'
   end
 
   scenario 'user can sign in with valid credentials' do
-    user = FactoryBot.create(:user)
+    
     signin(user.email, user.password)
     expect(page).to have_content 'Erfolgreich angemeldet'
   end
 
   scenario 'user cannot sign in with invalid email' do
-    user = FactoryBot.create(:user)
+    
     signin('invalid@mail.com', user.password)
     expect(page).to have_content 'E-Mail oder Passwort ungültig.'
   end
 
   scenario 'user canont sign in with invalid password' do
-    user = FactoryBot.create(:user)
+    
     signin(user.email, 'invalidpass')
     expect(page).to have_content 'E-Mail oder Passwort ungültig.'
   end
