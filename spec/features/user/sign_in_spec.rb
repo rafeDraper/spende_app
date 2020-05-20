96d5b4086db116d39c9016fe83bfed1842fca084
rafedraper <rafedraper@gmail.com>
2020-05-20 15:25:58 +0200

    spaces delete

--- spec/features/user/sign_in_spec.rb
+++ spec/features/user/sign_in_spec.rb
@@ -6,19 +6,16 @@ feature 'Sign in', :devise do
   end
 
   scenario 'user can sign in with valid credentials' do
-    
     signin(user.email, user.password)
     expect(page).to have_content 'Erfolgreich angemeldet'
   end
 
   scenario 'user cannot sign in with invalid email' do
-    
     signin('invalid@mail.com', user.password)
     expect(page).to have_content 'E-Mail oder Passwort ungültig.'
   end
 
   scenario 'user canont sign in with invalid password' do
-    
     signin(user.email, 'invalidpass')
     expect(page).to have_content 'E-Mail oder Passwort ungültig.'
   end
