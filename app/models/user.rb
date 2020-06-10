2805c01c10e7a58b92b6db913e8898850169fadf
rafedraper <rafedraper@gmail.com>
2020-06-10 11:35:56 +0200

    rubo-acab

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,8 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(provider_data)
-    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+    where(provider: provider_data.provider,
+          uid: provider_data.uid).first_or_create do |user|
       user.email = provider_data.info.email
       user.password = Devise.friendly_token[0, 20]
     end
