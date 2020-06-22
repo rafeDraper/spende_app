3382f4ed2af47aad37e08d0f4a55292459ade486
rafedraper <rafedraper@gmail.com>
2020-06-22 21:05:30 +0200

    green #delete controller test

--- spec/controllers/meetings_items_controller_spec.rb
+++ spec/controllers/meetings_items_controller_spec.rb
@@ -1,11 +1,12 @@
 RSpec.describe MeetingsItemsController, type: :controller do
+  before(:each) do
+    @item = FactoryBot.create(:meetings_item)
+    @list = FactoryBot.create(:meetings_list)
+  end
+
   describe '#new' do
     context 'authenticated user' do
       login_user
-      before do
-        @item = FactoryBot.create(:meetings_item)
-        @list = FactoryBot.create(:meetings_list)
-      end
 
       it 'responds successfully' do
         get :new, params: { meetings_list_id: @list.id }
@@ -17,17 +18,35 @@ RSpec.describe MeetingsItemsController, type: :controller do
   describe '#create' do
     context 'authenticated admin' do
       login_admin
-      before do
-        @item = FactoryBot.create(:meetings_item)
-        @list = FactoryBot.create(:meetings_list)
-      end
-      it 'succeed' do
+
+      it 'with valid input' do
         post :create, params: { meetings_item: {
           date: @item.date,
           amount: @item.amount,
           reason: @item.reason
         }, meetings_list_id: @list.id }
         expect(response).to redirect_to("/meetings_lists/#{@list.id}")
+      end
+
+      it 'with invalid input' do
+        post(:create, params: { meetings_item: {
+               date: nil,
+               amount: @item.amount,
+               reason: @item.reason
+             }, meetings_list_id: @list.id })
+        expect(response).to render_template(:new)
+        expect(response).to have_http_status('200')
+      end
+    end
+  end
+
+  describe '#delete' do
+    context 'authenticated admin' do
+      login_admin
+      it 'deletes an item' do
+        expect do
+          delete(:destroy, params: { id: @item.id, meetings_list_id: @item.id })
+        end.to change(MeetingsItem, :count).by(-1)
       end
     end
   end
